567ec9f083733c8c0c5f42449ceea2ee1066cb25
Santiago Lopez <103217292+itnasll@users.noreply.github.com>
2024-10-30 16:00:48 -0300

    caja fuerte con clave

--- Assets/Scripts/controlCajaFuerte.cs
+++ Assets/Scripts/controlCajaFuerte.cs
@@ -3,17 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Cinemachine;
+using System.Drawing;
 
-public class controlCajaFuerte : MonoBehaviour
+public class controlCajaFuerte : InteractObject
 {
-    public TMP_InputField inputField;  // Asigna el Input Field desde el Inspector
+      // Asigna el Input Field desde el Inspector
     public GameObject cajaFuerte;
     public Animator puerta;
     private bool estaAbierta = false;
     [SerializeField] string clave;
     [SerializeField] Canvas canvas;
+    [SerializeField] CinemachineVirtualCamera cameara;
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] TMP_Text mensajes;
+    string mensaje1 = "Clave correcta!";
+    string mensaje2 = "Clave incorrecta, intenta nuevamente.";
+
 
     //en el interactuar pregunta si esta abierta aplica la animacion cerrar
+
+    public override void Interact(PlayerViewController player)
+    {
+
+        if (estaAbierta)
+        {
+            cameara.Priority = 0;
+            inputField.DeactivateInputField();
+            cerrar();
+        }
+        else
+        {
+            cameara.Priority = 3;
+            
+            inputField.ActivateInputField();
+            
+
+        }
+
+
+    }
+
+    
+
     public void VerificarPalabra()
     {
         string textoIngresado = inputField.text;
@@ -21,28 +53,42 @@ public class controlCajaFuerte : MonoBehaviour
         // Verifica si la palabra ingresada es "Rubik"
         if (textoIngresado.Equals(clave, System.StringComparison.OrdinalIgnoreCase))
         {
+            mensajes.color = UnityEngine.Color.green;
+            mensajes.text = mensaje1;
+            Invoke("Borrar", 1f);
             Debug.Log("¡Palabra correcta! Abriendo la caja fuerte...");
+            inputField.DeactivateInputField();
             AbrirCajaFuerte();  // Llama a la función para abrir la caja fuerte
         }
         else
         {
-            Debug.Log("Palabra incorrecta, intenta nuevamente.");
+            mensajes.color = UnityEngine.Color.red;
+            mensajes.text = mensaje2;
+            Invoke("Borrar", 2f);
+            inputField.text = "";
+            inputField.ActivateInputField();
         }
     }
     public void cerrar()
     {
         puerta.Play("Cerrar");
+        inputField.DeactivateInputField();
     }
     private void AbrirCajaFuerte()
     {
         puerta.Play("Abrir");
         estaAbierta=true;
+        cameara.Priority = 0;
         //transform.Find("botones").gameObject.SetActive(false);
         // Aquí puedes poner la lógica para abrir la caja fuerte, como activar una animación o mover un objeto.
         //cajaFuerte.SetActive(false);  // Ejemplo: desactiva la caja fuerte
     }
-    public void Borrar()
+
+    private void Borrar()
     {
-        inputField.text = "";
+        mensajes.text =string.Empty;
+
     }
+
+    
 }
