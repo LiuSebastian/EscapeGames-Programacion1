22f9c9f3725b6733bce24028b909c4af9d6a0774
Santiago Lopez <103217292+itnasll@users.noreply.github.com>
2024-11-02 12:47:18 -0300

    grilla completa

--- Assets/pasifaeScript.cs
+++ Assets/pasifaeScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pasifaeScript : MonoBehaviour
+public class pasifaeScript : InteractObject
 {
     [SerializeField] Material Material;
     [SerializeField] Material Material1;
@@ -12,7 +12,7 @@ public class pasifaeScript : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] MeshRenderer meshRenderer;
 
-    
+    int vueltas = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +23,42 @@ public class pasifaeScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public override void Interact(PlayerViewController player)
+    {
+        Girar();
+    }
+    void Girar()
+    {
+        if (vueltas == 0)
+        {
+            animator.Play("giro1");
+            SetMaterial(vueltas);
+            vueltas++;
+        }
+        else if (vueltas == 1)
+        {
+            animator.Play("giro2");
+            SetMaterial(vueltas);
+            vueltas++;
+        }
+        else if (vueltas == 2)
+        {
+            animator.Play("giro3");
+            SetMaterial(vueltas);
+            vueltas++;
+        }
+        else
+        {
+            animator.Play("giro4");
+            SetMaterial(vueltas);
+            vueltas = 0;
+        }
+    }
+    void SetMaterial(int numero)
+    {
+        List<Material> materiales = new List<Material> { Material, Material1, Material2, Material3 };
+        meshRenderer.material = materiales[numero];
     }
 }
